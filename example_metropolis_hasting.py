8271f5058a8aa5436f45b6c7948084375ceecdb3
acruzgarcia <a.cruz.garcia@gmail.com>
2021-05-23 20:51:26 +0100

    small changes.

--- example_metropolis_hasting.py
+++ example_metropolis_hasting.py
@@ -8,37 +8,36 @@ from metropolis.metropolis import MetropolisAlgorithm
 # Please we aware that i am not generating here exactly the same random numbers than in R
 np.random.seed(568)
 x = np.random.normal(loc=0.0, scale=2.0, size=500)
+print('Sampled mean and std: mean={:2.2f}, std={:2.2f}'.format(np.mean(x), np.std(x)))
 
-log_prior_mean = lambda mu: stats.norm.logpdf(mu, loc=0, scale=0.5)
-log_prior_sigma = lambda sigma: stats.norm.logpdf(sigma, loc=2, scale=0.5)
-
-lupost = lambda param: np.sum(stats.norm.logpdf(x, loc=param[0], scale=np.exp(param[1]))) \
-                       * log_prior_mean(param[0]) \
-                       * log_prior_sigma(np.exp(param[1]))
-
+lupost = lambda param: np.sum(stats.norm.logpdf(x, loc=param[0], scale=np.exp(param[1])))
 metrop = MetropolisAlgorithm(lupost=lupost,
                              initial=np.array([-2, np.log(7)]),
                              scale=np.array([0.04, 0.04]),
                              burn_cont=100)
+
+# Sample the distribution of parameters
 result = metrop.run(batches=1000)
 
 # second parameter requires transform
 result.batch[:, 1] = np.exp(result.batch[:, 1])
 
-print('Sampled mean and std: mean={:2.2f}, std={:2.2f}'.format(np.mean(x), np.std(x)))
 means = [np.mean(result.batch[:, i]) for i in [0, 1]]
-print('Mean for parameters: mean={:2.2f}, std={:2.2f}'.format(*means))
 modes = [stats.mode(result.batch[:, i]).mode[0] for i in [0, 1]]
+
+# Print results
+print('Mean for parameters: mean={:2.2f}, std={:2.2f}'.format(*means))
 print('Mode for parameters: mean={:2.2f}, std={:2.2f}'.format(*modes))
 print('Accept rate is {:2.2f}'.format(result.accept))
 
-# Plot simulated params
+# Plot simulated markov chains, the parameters
 plt.figure()
 for i in [0, 1]:
     ax = plt.subplot(1, 2, i + 1)
     ax.plot(result.batch[:, i])
 plt.show()
 
+# Plot histograms of the parameters
 plt.figure()
 for i in [0, 1]:
     ax = plt.subplot(1, 2, i + 1)
